4041b63582367d43fe46c9433eddce1bba42bbc1
Dimon <rootcfg@mail.ru>
2022-04-03 20:53:59 +0300

    static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,10 +9,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 end
